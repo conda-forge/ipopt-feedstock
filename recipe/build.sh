867318f9a2254a2379d4159a5897bd97e4f5e1bd
Francesco Biscani <bluescarni@gmail.com>
2017-04-26 01:16:39 +0200

    Remove gcc_eh linking.

--- recipe/build.sh
+++ recipe/build.sh
@@ -20,7 +20,7 @@ fi
   CXXFLAGS=" -m64 -I$PREFIX/include -I$PREFIX/include/asl" \
   --with-blas-lib="-Wl,-rpath,$PREFIX/lib -L$PREFIX/lib -lopenblas" \
   --with-asl-lib="-Wl,-rpath,$PREFIX/lib -L$PREFIX/lib -lasl" \
-  --with-mumps-lib="-Wl,-rpath,$PREFIX/lib -L$PREFIX/lib -ldmumps -lmumps_common -lpord -lmpiseq -lesmumps -lscotch -lscotcherr -lmetis -lgfortran -lgcc_eh" \
+  --with-mumps-lib="-Wl,-rpath,$PREFIX/lib -L$PREFIX/lib -ldmumps -lmumps_common -lpord -lmpiseq -lesmumps -lscotch -lscotcherr -lmetis -lgfortran" \
   --prefix=$PREFIX
 
 make
