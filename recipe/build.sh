da0855e98ecf11ff0c0f43f053a4b044fc5b6d6c
Francesco Biscani <bluescarni@gmail.com>
2017-04-27 17:04:58 +0200

    OSX fixes (#5)
    Changes the compiler on OSX to clang++
    
    * Add SetNumericValue() call to the test.
    
    * First attempt at using clang on osx.
    
    * Remove gcc_eh linking.
    
    * Bump to 3.12.7.
    
    * Remove gcc from deps.
    
    * Build number update.
    
    * Fix sha256.
    
    * Try removing the mumps mpi patch.
    
    * Re-instate a modified version of the mumps patch.
    
    * Revert "Bump to 3.12.7."
    
    This reverts commit b330dafbf5c4004b5a88a0f00440f5fccff13b08.
    
    * MNT: Re-rendered with conda-smithy 2.3.0
    
    * Bump build number.
    
    * Account for review.
    
    * Review.
    
    * MNT: Re-rendered with conda-smithy 2.3.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,13 +8,11 @@ mkdir build
 cd build
 
 ../configure \
-  CXX=g++ \
-  CC=gcc \
   CFLAGS="-I$PREFIX/include -I$PREFIX/include/asl" \
   CXXFLAGS=" -m64 -I$PREFIX/include -I$PREFIX/include/asl" \
   --with-blas-lib="-Wl,-rpath,$PREFIX/lib -L$PREFIX/lib -lopenblas" \
   --with-asl-lib="-Wl,-rpath,$PREFIX/lib -L$PREFIX/lib -lasl" \
-  --with-mumps-lib="-Wl,-rpath,$PREFIX/lib -L$PREFIX/lib -ldmumps -lmumps_common -lpord -lmpiseq -lesmumps -lscotch -lscotcherr -lmetis -lgfortran -lgcc_eh" \
+  --with-mumps-lib="-Wl,-rpath,$PREFIX/lib -L$PREFIX/lib -ldmumps -lmumps_common -lpord -lmpiseq -lesmumps -lscotch -lscotcherr -lmetis -lgfortran" \
   --prefix=$PREFIX
 
 make
--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -6,15 +6,15 @@ ipopt mytoy.nl | grep -q "Optimal Solution"
 # Test linking against the ipopt library
 cd test
 
-g++ -I$PREFIX/include/coin  -c -o cpp_example.o cpp_example.cpp
-g++ -I$PREFIX/include/coin  -c -o MyNLP.o MyNLP.cpp
+${CXX} -I$PREFIX/include/coin  -c -o cpp_example.o cpp_example.cpp
+${CXX} -I$PREFIX/include/coin  -c -o MyNLP.o MyNLP.cpp
 
 # Deal with different linker flags
 if [ $(uname -s) == 'Darwin' ]; then
-  g++ -L$PREFIX/lib -Wl,-rpath,$PREFIX/lib -lipopt \
+  ${CXX} -L$PREFIX/lib -Wl,-rpath,$PREFIX/lib -lipopt \
     -I$PREFIX/include/coin -o cpp_example cpp_example.o MyNLP.o
 else
-  g++ -L$PREFIX/lib -lipopt -I$PREFIX/include/coin -o cpp_example cpp_example.o MyNLP.o
+  ${CXX} -L$PREFIX/lib -lipopt -I$PREFIX/include/coin -o cpp_example cpp_example.o MyNLP.o
 fi
 
 ./cpp_example | grep -q "Optimal Solution"
