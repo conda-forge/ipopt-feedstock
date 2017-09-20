006664247489dd80dc4dc982a6816aa02ce8bb0b
Richard Otis <richard.otis@outlook.com>
2017-09-20 14:38:16 -0700

    WIP: Revert compile test attempt

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,14 +3,7 @@ xcopy include\* %LIBRARY_INC% /sy
 xcopy lib\* %LIBRARY_LIB% /sy
 copy share\coin\doc\Ipopt\LICENSE LICENSE
 copy share\coin\doc\Ipopt\README README
-REM Test linking against the ipopt library
-REM Stopgap until conda-build#1059 is fixed
-cd test
-cl /c /I $PREFIX/include/coin cpp_example.cpp /out:cpp_example.obj
-cl /c /I $PREFIX/include/coin MyNLP.cpp /out:MyNLP.obj
-link /LIBPATH $PREFIX/lib cpp_example.obj MyNLP.obj /out:cpp_example.exe
-.\cpp_example.exe | findstr "Optimal Solution"
-del cpp_example.* MyNLP.*
+
 if errorlevel 1 exit 1
 
 :: Add more build steps here, if they are necessary.
