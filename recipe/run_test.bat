d878f8a6712e09eae7431e24860f906158262eff
Richard Otis <richard.otis@outlook.com>
2017-09-20 14:24:33 -0700

    WIP: Try moving the compile test into the build phase to work around conda/conda-build#1059

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -1,9 +1,2 @@
 REM Test the ipopt binary
 ipopt mytoy.nl | findstr "Optimal Solution"
-
-REM Test linking against the ipopt library
-cd test
-cl /c /I $PREFIX/include/coin cpp_example.cpp /out:cpp_example.obj
-cl /c /I $PREFIX/include/coin MyNLP.cpp /out:MyNLP.obj
-link /LIBPATH $PREFIX/lib /I $PREFIX/include/coin cpp_example.obj MyNLP.obj /out:cpp_example.exe
-.\cpp_example.exe | findstr "Optimal Solution"
