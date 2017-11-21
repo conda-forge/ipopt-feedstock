ce122681590e54a265ea71fe6ce0f6ea0516f3c7
Richard Otis <rotis@jpl.nasa.gov>
2017-11-21 15:32:16 -0800

    WIP: Windows source build support

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,8 +1,22 @@
-copy bin\ipopt.exe %LIBRARY_BIN%
 xcopy include\* %LIBRARY_INC% /sy
 xcopy lib\* %LIBRARY_LIB% /sy
 copy share\coin\doc\Ipopt\LICENSE LICENSE
 copy share\coin\doc\Ipopt\README README
+
+mkdir build
+cd build
+
+rm /mingw-w64/bin/make.exe
+bash -c "CXXDEFS=-DCOIN_USE_MUMPS_MPI_H ADD_CXXFLAGS='-I${PREFIX}/include -L${PREFIX}/lib' ../configure --build=x86_64-w64-mingw32 --disable-linear-solver-loader --with-blas-lib='-lopenblas' --with-mumps-lib='-ldmumps -lmumps_common -lpord -lgfortran' --enable-shared --enable-dependency-linking --prefix=${PREFIX}"
+cp /mingw-w64/bin/mingw32-make.exe /mingw-w64/bin/make.exe
+mingw32-make clean
+mingw32-make test
+mingw32-make libipopt.dll
+rm /mingw-w64/bin/make.exe
+
+mingw32-make install
+copy src\Interfaces\libipopt.dll %LIBRARY_LIB%
+
 
 if errorlevel 1 exit 1
 
--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -1,2 +1,9 @@
-REM Test the ipopt binary
-ipopt mytoy.nl | findstr "Optimal Solution"
+cd test
+
+${CXX} -I$PREFIX/include/coin  -c -o cpp_example.o cpp_example.cpp
+${CXX} -I$PREFIX/include/coin  -c -o MyNLP.o MyNLP.cpp
+
+${CXX} -L$PREFIX/lib -lipopt -I$PREFIX/include/coin -o cpp_example.exe cpp_example.o MyNLP.o
+
+cpp_example.exe | grep -q "Optimal Solution"
+
