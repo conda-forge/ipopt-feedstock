22ff263d35d022593849b601c21f88cee4b1195a
Peter St. John <peterc.stjohn@gmail.com>
2018-11-29 07:26:59 -0700

    mirnk suggested changes

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,7 +12,7 @@ cd build
   CXXFLAGS=" -m64 -I$PREFIX/include -I$PREFIX/include/asl -I$PREFIX/include/mumps_seq" \
   --with-blas-lib="-Wl,-rpath,$PREFIX/lib -L$PREFIX/lib -lopenblas" \
   --with-asl-lib="-Wl,-rpath,$PREFIX/lib -L$PREFIX/lib -lasl" \
-  --with-mumps-lib="-Wl,-rpath,$PREFIX/lib -L$PREFIX/lib -ldmumps_seq -lmumps_common -lpord -lmpiseq -lesmumps -lscotch -lscotcherr -lmetis -lgfortran" \
+  --with-mumps-lib="-Wl,-rpath,$PREFIX/lib -L$PREFIX/lib -ldmumps_seq -lmumps_common_seq -lpord_seq -lmpiseq_seq -lesmumps -lscotch -lscotcherr -lmetis -lgfortran" \
   --prefix=$PREFIX
 
 make
