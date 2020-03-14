1e9161a61d8d7cbb39f853ff910f4291b0c1940a
Julien Schueller <schueller@phimeca.com>
2020-03-14 11:25:45 +0100

    Update to 3.13.0

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -6,15 +6,15 @@ ipopt mytoy.nl | grep -q "Optimal Solution"
 # Test linking against the ipopt library
 cd test
 
-${CXX} -I$PREFIX/include/coin  -c -o cpp_example.o cpp_example.cpp
-${CXX} -I$PREFIX/include/coin  -c -o MyNLP.o MyNLP.cpp
+${CXX} -I$PREFIX/include/coin-or -c -o cpp_example.o cpp_example.cpp
+${CXX} -I$PREFIX/include/coin-or -c -o MyNLP.o MyNLP.cpp
 
 # Deal with different linker flags
 if [ $(uname -s) == 'Darwin' ]; then
   ${CXX} -L$PREFIX/lib -Wl,-rpath,$PREFIX/lib -lipopt \
-    -I$PREFIX/include/coin -o cpp_example cpp_example.o MyNLP.o
+    -I$PREFIX/include/coin-or -o cpp_example cpp_example.o MyNLP.o
 else
-  ${CXX} -L$PREFIX/lib -lipopt -I$PREFIX/include/coin -o cpp_example cpp_example.o MyNLP.o
+  ${CXX} -L$PREFIX/lib -lipopt -I$PREFIX/include/coin-or -o cpp_example cpp_example.o MyNLP.o
 fi
 
 ./cpp_example | grep -q "Optimal Solution"
