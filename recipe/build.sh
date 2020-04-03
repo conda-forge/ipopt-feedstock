a21ac10b336b0b7a060a08640507e6e531fc05e9
Julien Schueller <schueller@phimeca.com>
2020-04-03 15:09:35 +0200

    Also bundle metis

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# ipopt requires its own third parties now
+# ipopt looks only for its own ASL & Mumps now
 curl -fSsL https://github.com/coin-or-tools/ThirdParty-ASL/archive/releases/1.4.3.tar.gz | tar xz && cd ThirdParty-ASL-releases-1.4.3 && ./get.ASL && ./configure --prefix=${PREFIX} && make -j${CPU_COUNT} && make install && cd -
+
+curl -fSsL https://github.com/coin-or-tools/ThirdParty-Metis/archive/releases/1.3.9.tar.gz | tar xz && cd ThirdParty-Metis-releases-1.3.9 && ./get.Metis && ./configure --prefix=${PREFIX} && make -j${CPU_COUNT} && make install && cd -
 
 curl -fSsL https://github.com/coin-or-tools/ThirdParty-Mumps/archive/releases/1.6.2.tar.gz | tar xz && cd ThirdParty-Mumps-releases-1.6.2 && patch -p1 -i ${RECIPE_DIR}/thirdparty-mumps-linux-configure.patch && ./get.Mumps && ./configure --prefix=${PREFIX} && make -j${CPU_COUNT} && make install && cd -
 
