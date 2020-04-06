0bf2af419a099c4439b26671ae6e86742e222b1a
Julien Schueller <schueller@phimeca.com>
2020-04-06 18:22:08 +0200

    Define third party in meta.yaml

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # ipopt looks only for its own ASL & Mumps now
-curl -fSsL https://github.com/coin-or-tools/ThirdParty-ASL/archive/releases/1.4.3.tar.gz | tar xz && cd ThirdParty-ASL-releases-1.4.3 && ./get.ASL && ./configure --prefix=${PREFIX} && make -j${CPU_COUNT} && make install && cd -
+cd ASL && ./get.ASL && ./configure --prefix=${PREFIX} && make -j${CPU_COUNT} && make install && cd -
 
-curl -fSsL https://github.com/coin-or-tools/ThirdParty-Metis/archive/releases/1.3.9.tar.gz | tar xz && cd ThirdParty-Metis-releases-1.3.9 && ./get.Metis && ./configure --prefix=${PREFIX} && make -j${CPU_COUNT} && make install && cd -
+cd Metis && ./get.Metis && ./configure --prefix=${PREFIX} && make -j${CPU_COUNT} && make install && cd -
 
-curl -fSsL https://github.com/coin-or-tools/ThirdParty-Mumps/archive/releases/1.6.2.tar.gz | tar xz && cd ThirdParty-Mumps-releases-1.6.2 && patch -p1 -i ${RECIPE_DIR}/thirdparty-mumps-linux-configure.patch && ./get.Mumps && ./configure --prefix=${PREFIX} && make -j${CPU_COUNT} && make install && cd -
+cd Mumps && ./get.Mumps && ./configure --prefix=${PREFIX} && make -j${CPU_COUNT} && make install && cd -
+
+cd Ipopt
 
 if [ "$(uname)" == "Linux" ]; then
   export LDFLAGS="${LDFLAGS} -lrt"
