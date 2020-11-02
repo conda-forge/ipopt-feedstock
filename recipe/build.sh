8efed85adfe9e797abf65676060ebc8a442b649d
Jason K. Moore <moorepants@gmail.com>
2020-11-02 08:02:19 +0100

    Removed the vendored ASL software library.
    1) Packages shouldn't be vendored, new feedstocks should be produced and
    dependencies setup.
    2) ASL is under a custom license (not EPL) and may not be able to be
    included here.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# ipopt looks only for its own ASL & Mumps now
-cd ASL && ./get.ASL && ./configure --prefix=${PREFIX} && make -j${CPU_COUNT} && make install && cd -
-
+# ipopt looks only for its own Mumps now
 cd Mumps && ./get.Mumps && ./configure --prefix=${PREFIX} && make -j${CPU_COUNT} && make install && cd -
 
 cd Ipopt
