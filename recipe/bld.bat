e7f51e9136845a802a0904ec9e56798016e8cbb7
Phil Chiu <phil.chiu@archer.com>
2021-07-03 20:50:42 -0600

    Attempt to fix quotes for Windows build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,7 +8,7 @@ mkdir build
 cd build
 
 :: Configure using the CMakeFiles
-if %MKL_SUPPORT%=='nomkl' (
+if "%MKL_SUPPORT%" == "nomkl" (
       cmake -G "NMake Makefiles" ^
             -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
             -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
@@ -27,7 +27,7 @@ if %MKL_SUPPORT%=='nomkl' (
 )
 if errorlevel 1 exit 1
 
-if %MKL_SUPPORT%=='mkl' (
+if "%MKL_SUPPORT%" == "mkl"(
       cmake -G "NMake Makefiles" ^
             -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
             -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
